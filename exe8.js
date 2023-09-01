5d439279c62de45c147debb81cc19490d102131d
kaitou1412kid <kritagyashakya0@gmail.com>
2023-09-01 17:46:18 +0545

    exe8

---
+++ exe8.js
@@ -0,0 +1,10 @@
+function checkUpperCase(word){
+    if(word === word.toUpperCase()){
+        console.log("UpperCase");
+    }else {
+        console.log("Not uppercase")
+    }
+}
+
+checkUpperCase("kriatgya")
+checkUpperCase("HELLO")
